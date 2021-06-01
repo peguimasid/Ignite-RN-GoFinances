6853f8e4840c99c66544b324664eb1490908ab38
Guilhermo Masid <guilhermomasid@gmail.com>
2021-06-01 19:00:13 -0300

    create global styles

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,10 +4,11 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
   font-size: 20px;
-  color: #101010;
+  color: ${({ theme }) => theme.colors.title};
   font-weight: 500;
 `;
