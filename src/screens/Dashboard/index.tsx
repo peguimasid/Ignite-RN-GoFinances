3362a0b50dd6084cf60efb1655817c87ec38812f
Guilhermo Masid <guilhermomasid@gmail.com>
2021-06-01 18:32:40 -0300

    adding styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import { Text } from "react-native";
+
+import { Container, Title } from "./styles";
+
+export const Dashboard: FunctionComponent = () => {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+};
