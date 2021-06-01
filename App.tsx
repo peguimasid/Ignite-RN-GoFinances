7b67f41285eb170e6239592c9db9d001c289f1ba
Guilhermo Masid <guilhermomasid@gmail.com>
2021-06-01 17:56:50 -0300

    create dashboard component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
 import React from "react";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text>Hello World!</Text>
-    </View>
-  );
+  return <Dashboard />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import { View, Text } from "react-native";
+
+// import { Container } from './styles';
+
+export const Dashboard: FunctionComponent = () => {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
