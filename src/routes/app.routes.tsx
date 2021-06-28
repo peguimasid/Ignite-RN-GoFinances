fbe2149248b76cc8a55c6ffcfe8c20e751a44ecb
Guilhermo Masid <guilhermomasid@gmail.com>
2021-06-28 16:16:54 -0300

    styling tab navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,21 +1,68 @@
 import React from 'react';
 import { FunctionComponent } from 'react';
-import { View } from 'react-native';
+
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { useTheme } from 'styled-components';
 
 import { Register } from '../screens/Register';
 import { Dashboard } from '../screens/Dashboard';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Platform } from 'react-native';
 
 // import { Container } from './styles';
 
 export const AppRoutes: FunctionComponent = () => {
   const { Navigator, Screen } = createBottomTabNavigator();
 
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="Dashboard" component={Dashboard} />
-      <Screen name="Register" component={Register} />
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.secondary,
+        inactiveTintColor: theme.colors.text,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 80,
+        },
+      }}
+    >
+      <Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Register"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Resume"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 };
